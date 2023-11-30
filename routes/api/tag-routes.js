433bd639480672d6a22e3ea2e8436f6823825c3f
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-30 08:11:39 -0700

    updated all models in models folder// updated the paths for routes in routes/api folder// added info for .env login

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,25 +3,25 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/api/tags', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/api/tags/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/api/tags/:id', (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/api/tags/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/api/tags/:id', (req, res) => {
   // delete on tag by its `id` value
 });
 
